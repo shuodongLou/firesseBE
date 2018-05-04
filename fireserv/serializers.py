262b473aa5eefacb9aadc52ff0a4cb3a0897b044
Shuodong Lou <chenfei_lou@sina.com>
2018-05-04 20:23:22 +0800

    changed photo extention method

--- fireserv/serializers.py
+++ fireserv/serializers.py
@@ -65,19 +65,19 @@ class AccountSerializer(serializers.ModelSerializer):
 class Base64ImageField(serializers.ImageField):
 
     def to_internal_value(self, data):
-
         # Check if this is a base64 string
         if isinstance(data, six.string_types):
             # Check if the base64 string is in the "data:" format
             if 'data:' in data and ';base64,' in data:
             	# Break out the header from the base64 content
-            	header, data = data.split(';base64,')
+                header, data = data.split(';base64,')
+
 
             # Try to decode the file. Return validation error if it fails.
             try:
-            	decoded_file = base64.b64decode(data)
+                decoded_file = base64.b64decode(data)
             except TypeError:
-            	self.fail('invalid_image')
+                self.fail('invalid_image')
 
             # Generate file name:
             file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
@@ -91,11 +91,15 @@ class Base64ImageField(serializers.ImageField):
         return super(Base64ImageField, self).to_internal_value(data)
 
     def get_file_extension(self, file_name, decoded_file):
+        def test_jpeg(h, f):
+            """JPEG data in JFIF or Exif format"""
+            if h[6:10] in (b'JFIF', b'Exif') or h[:2] == b'\xff\xd8':
+                return 'jpeg'
+        imghdr.tests.append(test_jpeg)
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
 
-    	extension = imghdr.what(file_name, decoded_file)
-    	extension = "jpg" if extension == "jpeg" else extension
-
-    	return extension
+        return extension
 
 class PhotoSerializer(serializers.ModelSerializer):
 
